e67b5b2752d49dfdb71b43e6b1dd2ae8ecee66a9
Anastasia <70826807+Juliet94@users.noreply.github.com>
2021-10-14 18:22:47 +0300

    Настроит сборку

---
+++ src/components/main-page/main-page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './main-page.module.scss';
+
+function MainPage() {
+  return (
+    <div className={styles.wrapper} />
+  );
+}
+
+export default MainPage;
