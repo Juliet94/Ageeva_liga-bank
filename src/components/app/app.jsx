0e9effb2207d71f8bbd6b6c67e69dbb3fc8abdad
Anastasia <70826807+Juliet94@users.noreply.github.com>
2021-10-14 18:24:17 +0300

    Настроит сборку

---
+++ src/components/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainPage from '../main-page/main-page';
+import './app.module.scss';
+
+function App() {
+  return (
+    <MainPage />
+  );
+}
+
+export default App;
+
