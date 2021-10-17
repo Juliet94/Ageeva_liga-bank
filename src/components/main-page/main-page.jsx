4c53f1746b34d8b1181c599a2382a9672387b384
Anastasia <Juliet94@users.noreply.github.com>
2021-10-17 11:54:22 +0300

    Добавит слайдер WIP

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import styles from './main-page.module.scss';
 
 import Header from '../header/header';
+import Slider from '../slider/slider';
 
 function MainPage() {
   return (
     <div className={styles.wrapper}>
       <Header />
+      <main>
+        <h1 className="visually-hidden">
+          Кредитный калькулятор Лига Банка
+        </h1>
+        <Slider />
+      </main>
     </div>
   );
 }
